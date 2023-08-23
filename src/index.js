c2d785370fca1da81f294c173e488d80a5546f49
gnkm <genki.matsunaga@gmail.com>
2023-08-23 22:26:04 +0900

    Add a script

---
+++ src/index.js
@@ -0,0 +1,63 @@
+import * as fs from 'node:fs/promises';
+import { parseArgs } from 'node:util';
+import rehypeDocument from 'rehype-document';
+import rehypeFormat from 'rehype-format';
+import rehypeKatex from 'rehype-katex';
+import rehypeStringify from 'rehype-stringify';
+import rehypeMathjax from 'rehype-mathjax';
+import remarkMath from 'remark-math';
+import remarkParse from 'remark-parse';
+import remarkRehype from 'remark-rehype';
+import { unified } from 'unified';
+import { read } from 'to-vfile';
+
+const { values, positionals } = parseArgs({
+  options: {
+    file: {
+      type: 'string',
+      short: 'f',
+    },
+  },
+});
+const inputFileName = values['file'];
+
+const contentNoMath = await unified()
+  .use(remarkParse)
+  .use(remarkRehype)
+  .use(rehypeFormat)
+  .use(rehypeStringify)
+  .process(await read(inputFileName));
+
+const contentSimpleMath = await unified()
+  .use(remarkParse)
+  .use(remarkMath)
+  .use(remarkRehype)
+  .use(rehypeFormat)
+  .use(rehypeStringify)
+  .process(await read(inputFileName));
+
+const contentKatex = await unified()
+  .use(remarkParse)
+  .use(remarkMath)
+  .use(remarkRehype)
+  .use(rehypeKatex)
+  .use(rehypeDocument, {
+    css: 'https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.css',
+  })
+  .use(rehypeFormat)
+  .use(rehypeStringify)
+  .process(await read(inputFileName));
+
+const contentMathjax = await unified()
+  .use(remarkParse)
+  .use(remarkMath)
+  .use(remarkRehype)
+  .use(rehypeMathjax)
+  .use(rehypeFormat)
+  .use(rehypeStringify)
+  .process(await read(inputFileName));
+
+fs.writeFile('html/no-math.html', String(contentNoMath));
+fs.writeFile('html/simple-math.html', String(contentSimpleMath));
+fs.writeFile('html/katex.html', String(contentKatex));
+fs.writeFile('html/mathjax.html', String(contentMathjax));
